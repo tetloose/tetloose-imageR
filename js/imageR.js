6574e522a635ad729360a308cf563ed751cf957e
tetloose <tetloose@gmail.com>
2017-01-28 19:04:48 +0000

    copywrite stuff

--- js/imageR.js
+++ js/imageR.js
@@ -1,3 +1,18 @@
+/*
+ ______   ______     ______   __         ______     ______     ______     ______     ______     ______     __    __
+/\__  _\ /\  ___\   /\__  _\ /\ \       /\  __ \   /\  __ \   /\  ___\   /\  ___\   /\  ___\   /\  __ \   /\ "-./  \
+\/_/\ \/ \ \  __\   \/_/\ \/ \ \ \____  \ \ \/\ \  \ \ \/\ \  \ \___  \  \ \  __\   \ \ \____  \ \ \/\ \  \ \ \-./\ \
+   \ \_\  \ \_____\    \ \_\  \ \_____\  \ \_____\  \ \_____\  \/\_____\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \ \_\
+    \/_/   \/_____/     \/_/   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/   \/_/  \/_/
+                                                                                                                             Version: 1.0
+Author: James Tetley
+Website: http://tetloose.com
+Docs: https://github.com/tetloose/imageR
+Repo: https://github.com/tetloose/imageR
+Issues: https://github.com/tetloose/imageR/issues
+FreeToUse: Yep
+*/
+
 (function( $ ) {
 
     $.fn.imageR = function() {
